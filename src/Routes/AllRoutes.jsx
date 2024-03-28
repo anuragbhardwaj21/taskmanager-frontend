1a0286e104cf9da9c328d3c881056594c5a94e2e
Anurag Bhardwaj <anuragmac@Anurags-MacBook-Air.local>
2024-03-28 12:00:36 +0530

    update

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import { Homepage } from "../Pages/Homepage";
 import { Login } from "../Pages/Login";
 import { Signup } from "../Pages/Signup";
 import { NotFoundPage } from "../Pages/NotFoundPage";
 import { CreateTask } from "../Pages/CreateTask";
-
+import { useSelector } from "react-redux";
 const AllRoutes = () => {
+  let isAuthenticated = useSelector(
+    (store) => store.userReducer.isAuthenticated
+  );
   return (
     <Router>
       <Routes>
@@ -16,6 +24,9 @@ const AllRoutes = () => {
         <Route exact path="/signup" element={<Signup />} />
         <Route exact path="/createtask" element={<CreateTask />} />
         <Route path="*" element={<NotFoundPage />} />
+        {!isAuthenticated && (
+          <Route path="*" element={<Navigate to="/login" />} />
+        )}
       </Routes>
     </Router>
   );
