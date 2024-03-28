6e0cc12c84adf693fef77768fb8312cac1b1f886
Anurag Bhardwaj <anuragmac@Anurags-MacBook-Air.local>
2024-03-28 12:03:10 +0530

    update

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,32 +1,30 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import { Homepage } from "../Pages/Homepage";
 import { Login } from "../Pages/Login";
 import { Signup } from "../Pages/Signup";
 import { NotFoundPage } from "../Pages/NotFoundPage";
 import { CreateTask } from "../Pages/CreateTask";
 import { useSelector } from "react-redux";
+
 const AllRoutes = () => {
-  let isAuthenticated = useSelector(
-    (store) => store.userReducer.isAuthenticated
-  );
+  const isAuthenticated = useSelector((store) => store.userReducer.isAuthenticated);
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/dashboard" />} />
+        {/* Redirect to dashboard if user is authenticated, otherwise redirect to login */}
+        <Route
+          path="/"
+          element={
+            isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/login" />
+          }
+        />
         <Route exact path="/dashboard" element={<Homepage />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/signup" element={<Signup />} />
         <Route exact path="/createtask" element={<CreateTask />} />
         <Route path="*" element={<NotFoundPage />} />
-        {!isAuthenticated && (
-          <Route path="*" element={<Navigate to="/login" />} />
-        )}
       </Routes>
     </Router>
   );
